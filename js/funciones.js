c1832ed1305396cf1f25e39beaa7ed7eb5174598
RABIAZEEM <rabiaazeem48@gmail.com>
2022-07-20 11:07:33 +0200

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+function playAudio(idAudio){
+    let elemAudio=document.getElementById(idAudio);
+    elemAudio.play();
+}
+//function ocultaMuestra(oculta,muestra){
+  //  let strOculta = oculta;
+  //  let strMuestra = muestra;
+  //  document.getElementsByClassName(strOculta).style.display="none";
+  //  document.getElementsByClassName(strMuestra).style.display="block";
+//}
